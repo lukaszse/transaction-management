b8fda268259c437f3d17ed927bb1a5d1fd9b89ed
lukaszse <llseremak@gmail.com>
2021-10-11 18:21:17 +0200

    initial commit

---
+++ src/main/java/pl/com/seremak/simplebills/SimpleBillsApplication.java
@@ -0,0 +1,13 @@
+package pl.com.seremak.simplebills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/seremak/simplebills/SimpleBillsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.seremak.simplebills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
