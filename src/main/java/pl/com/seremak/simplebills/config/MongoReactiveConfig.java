79fd6d81cd215f8e8292d028a856bcd5f419f46e
lukaszse <llseremak@gmail.com>
2021-10-16 13:27:42 +0200

    add missing MongoReactiveConfig config

--- src/main/java/pl/com/seremak/simplebills/config/MongoReactiveConfig.java
+++ src/main/java/pl/com/seremak/simplebills/config/MongoReactiveConfig.java
@@ -2,6 +2,7 @@ package pl.com.seremak.simplebills.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
@@ -9,7 +10,7 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 import org.springframework.lang.NonNull;
 
 @Configuration
-@EnableReactiveMongoRepositories
+@EnableReactiveMongoRepositories(basePackages = "pl.com.seremak.simplebills.repository")
 public class MongoReactiveConfig extends AbstractReactiveMongoConfiguration {
 
     public static final String SIMPLE_BILL_MONGO_DATABASE = "SimpleBill";
