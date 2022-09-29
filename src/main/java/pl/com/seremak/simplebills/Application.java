8ef8f4a393b64bea18b10531fb9d12a03c530f61
lseremak <llseremak@gmail.com>
2022-09-29 21:15:29 +0200

    update

--- src/main/java/pl/com/seremak/simplebills/Application.java
+++ src/main/java/pl/com/seremak/simplebills/Application.java
@@ -2,9 +2,13 @@ package pl.com.seremak.simplebills;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class, MongoReactiveDataAutoConfiguration.class, MongoReactiveAutoConfiguration.class})
 @EnableWebFlux
 public class Application {
 
