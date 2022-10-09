dae6db3e989236bab76cec3758f7b3bc71b87a96
lseremak <llseremak@gmail.com>
2022-10-09 18:13:03 +0200

    add various environment config

--- src/main/java/pl/com/seremak/simplebills/config/SpringSecurity.java
+++ src/main/java/pl/com/seremak/simplebills/config/SpringSecurity.java
@@ -2,6 +2,7 @@ package pl.com.seremak.simplebills.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -16,9 +17,24 @@ public class SpringSecurity {
         return http.authorizeExchange()
                 .pathMatchers("/users/admin", "/users/admin/*").hasAuthority("ROLE_ADMIN")
                 .anyExchange().permitAll()
-                .and().httpBasic()
-                .and().cors().disable()
+                .and()
+                .oauth2ResourceServer().jwt()
+                .and().and()
+                .cors().disable()
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
+
+//        return http.authorizeExchange()
+//                .pathMatchers("/users/admin", "/users/admin/*").hasAuthority("ROLE_ADMIN")
+//                .anyExchange().hasAuthority("SCOPE_write")
+//                .and()
+//                .cors()
+//                .and()
+//                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+//                .oauth2ResourceServer()
+//                .jwt()
+//                .and()
+//                .and()
+//                .build();
     }
 }
