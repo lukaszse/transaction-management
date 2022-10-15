1b227ba261a8576e4d7782eb50f966fd68191840
lseremak <llseremak@gmail.com>
2022-10-15 22:33:59 +0200

    configure cors

--- src/main/java/pl/com/seremak/simplebills/config/SpringSecurity.java
+++ src/main/java/pl/com/seremak/simplebills/config/SpringSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
@@ -18,13 +19,15 @@ import java.util.List;
 public class SpringSecurity {
 
     @Value("${simpleBillsGuiAppUrl}")
-    private  String simpleBillsGuiApp;
+    private String simpleBillsGuiApp;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(
             ServerHttpSecurity http) {
         return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .csrf()
+                .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeExchange()
                 .pathMatchers("/users/admin", "/users/admin/*").hasAuthority("ROLE_ADMIN")
                 .anyExchange().permitAll()
@@ -41,8 +44,7 @@ public class SpringSecurity {
         corsConfig.addAllowedMethod(HttpMethod.PUT);
         corsConfig.addAllowedMethod(HttpMethod.DELETE);
         corsConfig.setAllowedOrigins(List.of(simpleBillsGuiApp));
-        final UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
         return source;
     }
