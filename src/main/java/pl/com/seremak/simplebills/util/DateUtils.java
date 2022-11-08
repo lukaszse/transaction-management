e3c1c5f7621ff18c9f012cd059a6e51fae60ce50
lseremak <llseremak@gmail.com>
2022-11-08 12:11:53 +0100

    ipdate DateUtils

--- src/main/java/pl/com/seremak/simplebills/util/DateUtils.java
+++ src/main/java/pl/com/seremak/simplebills/util/DateUtils.java
@@ -1,7 +1,10 @@
 package pl.com.seremak.simplebills.util;
 
+import com.mongodb.lang.Nullable;
+
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.ZoneId;
 import java.util.Optional;
 
@@ -9,13 +12,19 @@ public class DateUtils {
 
     public static final ZoneId ZONE_UTC = ZoneId.of("UTC");
 
-    public static Optional<Instant> toInstantUTC(final LocalDate localDate) {
+    public static Optional<Instant> toInstantUTC(@Nullable final LocalDate localDate) {
         return Optional.ofNullable(localDate)
                 .map(presentLocalDate -> presentLocalDate.atStartOfDay(ZONE_UTC).toInstant());
     }
 
-    public static Optional<LocalDate> toLocalDate(final Instant instant) {
+    public static Optional<LocalDate> toLocalDate(@Nullable final Instant instant) {
         return Optional.ofNullable(instant)
                 .map(presentInstant -> LocalDate.ofInstant(presentInstant, ZONE_UTC));
+    }
+
+    public static Optional<YearMonth> toYearMonth(final Instant instant) {
+        return Optional.ofNullable(instant)
+                .flatMap(DateUtils::toLocalDate)
+                .map(localDate -> YearMonth.of(localDate.getYear(), localDate.getMonth()));
     }
 }
